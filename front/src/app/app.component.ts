b54a38d70ca10743a6026c5d0e7da8d936acb002
Andréa Franco <dedefac96@gmail.com>
2024-08-26 14:43:31 +0200

    Modified the app component: added the sidenav and different displays if logged in

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'front';
+
+  public $isLogged(): Observable<boolean> {
+    return of(true);
+  }
+
 }
