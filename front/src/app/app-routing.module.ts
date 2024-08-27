83faa4eb5988f69e61a77f292047047749648799
Andréa Franco <dedefac96@gmail.com>
2024-08-27 13:24:23 +0200

    changed route from posts to feed

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ProfileComponent } from './pages/profile/profile.component';
-import { PostsComponent } from './pages/posts/posts.component';
 import { TopicsComponent } from './pages/topics/topics.component';
+import { FeedComponent } from './features/posts/components/feed/feed.component';
 
 // consider a guard combined with canLoad / canActivate route option
 // to manage unauthenticated user to access private routes
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'profile', component: ProfileComponent },
-  { path: 'posts', component: PostsComponent },
+  { path: 'feed', component: FeedComponent },
   { path: 'topics', component: TopicsComponent },
 ];
 
