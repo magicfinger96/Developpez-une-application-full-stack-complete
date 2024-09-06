0763f754be5f0f1efc3e043c2b9d84ee71fb9317
Andréa Franco <dedefac96@gmail.com>
2024-09-06 14:41:15 +0200

    Load ProfileComponent when the url path is profile

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -5,12 +5,13 @@ import { FeedComponent } from './features/posts/components/feed/feed.component';
 import { DetailsComponent } from './features/posts/components/details/details.component';
 import { ListComponent } from './features/topics/components/list/list.component';
 import { CreateComponent } from './features/posts/components/create/create.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 // consider a guard combined with canLoad / canActivate route option
 // to manage unauthenticated user to access private routes
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'profile', component: ListComponent },
+  { path: 'profile', component: ProfileComponent },
   { path: 'feed', component: FeedComponent },
   { path: 'feed/details/:id', component: DetailsComponent },
   { path: 'topics', component: ListComponent },
