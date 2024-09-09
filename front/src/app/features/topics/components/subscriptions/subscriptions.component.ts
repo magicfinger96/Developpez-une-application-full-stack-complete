106a6ea588ec59a26a01de780f4a68bdb21ae727
Andréa Franco <dedefac96@gmail.com>
2024-09-09 13:22:40 +0200

    Added the subscriptions component

---
+++ front/src/app/features/topics/components/subscriptions/subscriptions.component.ts
@@ -0,0 +1,44 @@
+import { Component, inject } from '@angular/core';
+import { TopicCardComponent } from '../topic-card/topic-card.component';
+import { CommonModule } from '@angular/common';
+import { TopicsService } from '../../services/topics.service';
+import { SubscriptionsService } from '../../services/subscriptions.service';
+import { forkJoin, map, Observable } from 'rxjs';
+import { Topic } from '../../interfaces/topic.interface';
+
+@Component({
+  selector: 'app-subscriptions',
+  standalone: true,
+  imports: [TopicCardComponent, CommonModule],
+  templateUrl: './subscriptions.component.html',
+  styleUrl: './subscriptions.component.scss',
+})
+export class SubscriptionsComponent {
+  private topicsService: TopicsService = inject(TopicsService);
+  private subscriptionsService: SubscriptionsService =
+    inject(SubscriptionsService);
+
+  public topics$ = this.topicsService.all();
+  public subscribedTopics$!: Observable<Topic[]>;
+
+  ngOnInit() {
+    this.loadTopics();
+  }
+
+  private loadTopics(): void {
+    this.subscribedTopics$ = forkJoin([
+      this.subscriptionsService.mine(),
+      this.topicsService.all(),
+    ]).pipe(
+      map(([subscriptions, topics]) => {
+        {
+          return topics.filter((topic) =>
+            subscriptions.find(
+              (subscription) => subscription.topic_id === topic.id
+            )
+          );
+        }
+      })
+    );
+  }
+}
