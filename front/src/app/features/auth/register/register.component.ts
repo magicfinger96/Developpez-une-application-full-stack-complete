f52a8daad31718766afeb22545c4665d898f4eae
Andréa Franco <dedefac96@gmail.com>
2024-09-11 10:31:34 +0200

    added header on login and register pages

--- front/src/app/features/auth/register/register.component.ts
+++ front/src/app/features/auth/register/register.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../../core/components/header/header.component";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
