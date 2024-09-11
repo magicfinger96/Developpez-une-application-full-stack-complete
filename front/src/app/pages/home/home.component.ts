c630734bda7c35cc0705b5a75f92aee6e3da8be0
Andréa Franco <dedefac96@gmail.com>
2024-09-11 10:24:20 +0200

    updated home buttons and link them to the routes

--- front/src/app/pages/home/home.component.ts
+++ front/src/app/pages/home/home.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
+  imports: [MatButton, RouterLink],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
