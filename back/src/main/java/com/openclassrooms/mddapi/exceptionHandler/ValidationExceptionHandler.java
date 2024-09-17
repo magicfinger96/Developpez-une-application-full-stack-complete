eb31def5891473727f884aa12283c4247f64855b
Andréa Franco <dedefac96@gmail.com>
2024-09-17 17:22:34 +0200

    created the validation exception handler

---
+++ back/src/main/java/com/openclassrooms/mddapi/exceptionHandler/ValidationExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.openclassrooms.mddapi.exceptionHandler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class ValidationExceptionHandler {
+
+    /**
+     * Returns a readable error response when the validation of input failed.
+     *
+     * @param exception exception generated from the call.
+     * @return an error ResponseEntity.
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+        exception.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+}
