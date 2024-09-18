106fd33ffb0d6d2db297aa13abd41b6ea29de53e
Andréa Franco <dedefac96@gmail.com>
2024-09-18 11:59:14 +0200

    Fixed typo

--- back/src/main/java/com/openclassrooms/mddapi/MddApiApplication.java
+++ back/src/main/java/com/openclassrooms/mddapi/MddApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MddapiApplication {
+public class MddApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MddapiApplication.class, args);
+		SpringApplication.run(MddApiApplication.class, args);
 	}
 
 }
