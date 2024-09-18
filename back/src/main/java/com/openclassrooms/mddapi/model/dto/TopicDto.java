36b32371631756a6af8c5c36de2188780d9bea64
Andréa Franco <dedefac96@gmail.com>
2024-09-18 17:50:26 +0200

    added topic dto

---
+++ back/src/main/java/com/openclassrooms/mddapi/model/dto/TopicDto.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.mddapi.model.dto;
+
+import lombok.Data;
+
+/**
+ * DTO of a topic.
+ */
+@Data
+public class TopicDto {
+    int id;
+    String title;
+    String description;
+}
