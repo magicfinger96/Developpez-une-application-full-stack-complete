96ce35be0b0ac84923a1ed37fe226403367fe0a1
Andréa Franco <dedefac96@gmail.com>
2024-09-18 11:59:30 +0200

    added documentation

--- back/src/main/java/com/openclassrooms/mddapi/exception/AlreadyUsedEmailException.java
+++ back/src/main/java/com/openclassrooms/mddapi/exception/AlreadyUsedEmailException.java
@@ -1,5 +1,8 @@
 package com.openclassrooms.mddapi.exception;
 
+/**
+ * Exception thrown when the email is already used.
+ */
 public class AlreadyUsedEmailException extends Exception {
     public AlreadyUsedEmailException(String errorMessage) {
         super(errorMessage);
--- back/src/main/java/com/openclassrooms/mddapi/exception/AlreadyUsedUsernameException.java
+++ back/src/main/java/com/openclassrooms/mddapi/exception/AlreadyUsedUsernameException.java
@@ -1,5 +1,8 @@
 package com.openclassrooms.mddapi.exception;
 
+/**
+ * Exception thrown when the username is already used.
+ */
 public class AlreadyUsedUsernameException extends Exception {
     public AlreadyUsedUsernameException(String errorMessage) {
         super(errorMessage);
