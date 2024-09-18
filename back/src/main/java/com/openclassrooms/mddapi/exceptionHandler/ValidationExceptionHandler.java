e94532653568b390c1ea95603f8d4886235eb7d7
Andréa Franco <dedefac96@gmail.com>
2024-09-18 12:12:26 +0200

    display only the first error in the validationexceptionhandler

--- back/src/main/java/com/openclassrooms/mddapi/exceptionHandler/ValidationExceptionHandler.java
+++ back/src/main/java/com/openclassrooms/mddapi/exceptionHandler/ValidationExceptionHandler.java
@@ -2,13 +2,9 @@ package com.openclassrooms.mddapi.exceptionHandler;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @ControllerAdvice
 public class ValidationExceptionHandler {
@@ -20,13 +16,7 @@ public class ValidationExceptionHandler {
      * @return an error ResponseEntity.
      */
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException exception) {
-        Map<String, String> errors = new HashMap<>();
-        exception.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    public ResponseEntity<String> handleValidationExceptions(MethodArgumentNotValidException exception) {
+        return new ResponseEntity<>(exception.getBindingResult().getAllErrors().get(0).getDefaultMessage(), HttpStatus.BAD_REQUEST);
     }
 }
