cdaa0eda6a4a0c430149f45b0c46812bec540f95
Andréa Franco <dedefac96@gmail.com>
2024-09-20 16:01:33 +0200

    implemented the feed endpoint

---
+++ back/src/main/java/com/openclassrooms/mddapi/controller/PostController.java
@@ -0,0 +1,53 @@
+package com.openclassrooms.mddapi.controller;
+
+import com.openclassrooms.mddapi.exception.UserNotFoundException;
+import com.openclassrooms.mddapi.model.dto.PostDto;
+import com.openclassrooms.mddapi.model.dto.TopicDto;
+import com.openclassrooms.mddapi.model.entity.Post;
+import com.openclassrooms.mddapi.model.response.MessageResponse;
+import com.openclassrooms.mddapi.service.AuthenticationService;
+import com.openclassrooms.mddapi.service.PostService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Handles the end points related to the posts.
+ */
+@RestController
+public class PostController {
+
+    @Autowired
+    private PostService postService;
+
+    @Autowired
+    private AuthenticationService authenticationService;
+
+
+    /**
+     * End point that provides the feed of the authenticated user.
+     * A feed being the posts which belongs to topics the user subscribed to.
+     *
+     * @param sort  the post data to sort.
+     * @param order the sort order: ascending or descending.
+     * @return a ResponseEntity containing the posts if the call succeeded.
+     * Otherwise, returns an error ResponseEntity.
+     */
+    @GetMapping("/api/posts")
+    public ResponseEntity<PostDto[]> getFeed(@RequestParam(defaultValue = "createdAt") String sort,
+                                             @RequestParam(defaultValue = "desc") String order) {
+        try {
+            int userId = authenticationService.getAuthenticatedUserId();
+
+            Sort.Direction direction = "asc".equalsIgnoreCase(order) ? Sort.Direction.ASC : Sort.Direction.DESC;
+            return ResponseEntity.ok(postService.getFeed(userId, Sort.by(direction, sort)));
+
+        } catch (UserNotFoundException exception) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+    }
+}
+
---
+++ back/src/main/java/com/openclassrooms/mddapi/service/PostService.java
@@ -0,0 +1,35 @@
+package com.openclassrooms.mddapi.service;
+
+import com.openclassrooms.mddapi.model.dto.PostDto;
+import com.openclassrooms.mddapi.model.entity.Post;
+import com.openclassrooms.mddapi.repository.PostRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+/**
+ * Service which handles posts logic.
+ */
+@Service
+public class PostService {
+    @Autowired
+    private PostRepository postRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    /**
+     * Get the feed of the user: all the posts belonging to topics the user is subscribed to.
+     *
+     * @param userId id of the user.
+     * @param sort   object that sorts the result.
+     * @return an array of PostDto.
+     */
+    public PostDto[] getFeed(int userId, Sort sort) {
+        Collection<Post> posts = postRepository.findPostsFromSubscribedTopicByUser(userId, sort);
+        return posts.stream().map(post -> modelMapper.map(post, PostDto.class)).toList().toArray(PostDto[]::new);
+    }
+}
