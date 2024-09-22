4b01ae4d99b477513fe1056b0b831e9100ed6770
Andréa Franco <dedefac96@gmail.com>
2024-09-22 19:10:30 +0200

    [front] refacto: use observable inside the .ts to not make several api call

--- front/src/app/features/posts/components/feed/feed.component.ts
+++ front/src/app/features/posts/components/feed/feed.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { PostsService } from '../../services/posts.service';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
@@ -7,6 +7,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { ScrollingModule } from '@angular/cdk/scrolling';
 import { PostCardComponent } from '../post-card/post-card.component';
 import { RouterLink } from '@angular/router';
+import { Post } from '../../interfaces/post.interface';
+import { Subscription } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-feed',
@@ -18,19 +21,44 @@ import { RouterLink } from '@angular/router';
     MatButtonModule,
     ScrollingModule,
     PostCardComponent,
-    RouterLink
+    RouterLink,
   ],
   templateUrl: './feed.component.html',
   styleUrl: './feed.component.scss',
 })
-export class FeedComponent {
+export class FeedComponent implements OnInit, OnDestroy {
   private postsService: PostsService = inject(PostsService);
+  private feedSubscription: Subscription = new Subscription();
+  private matSnackBar: MatSnackBar = inject(MatSnackBar);
 
-  public sortOrder : string = "desc";
-  public feed$ = this.postsService.feed();
+  public sortOrder: string = 'desc';
+  public posts!: Post[];
+
+  public ngOnInit(): void {
+    this.fetchFeed(this.sortOrder);
+  }
+
+  public ngOnDestroy(): void {
+    this.feedSubscription.unsubscribe();
+  }
 
   public onSortClicked(): void {
-    this.sortOrder = this.sortOrder === "asc" ? "desc" : "asc";
-    this.feed$ = this.postsService.feed(this.sortOrder);
+    this.sortOrder = this.sortOrder === 'asc' ? 'desc' : 'asc';
+    this.fetchFeed(this.sortOrder);
+  }
+
+  private fetchFeed(sortOrder: string): void {
+    this.feedSubscription.unsubscribe();
+    this.feedSubscription = this.postsService.feed(sortOrder).subscribe({
+      next: (posts: Post[]) => {
+        this.posts = posts;
+      },
+      error: (errorResponse) =>
+        this.matSnackBar.open(
+          `Une erreur est survenue lors de la récupération des articles.`,
+          'Close',
+          { duration: 3000 }
+        ),
+    });
   }
 }
