9080fe6ddfe5a03e13433b6fc4e39a84f62988ef
Andréa Franco <dedefac96@gmail.com>
2024-09-25 20:47:59 +0200

    [back] added httpmessagenotreadable exception in the exception handler and always returns message response

--- back/src/main/java/com/openclassrooms/mddapi/exceptionHandler/ExceptionHandler.java
+++ back/src/main/java/com/openclassrooms/mddapi/exceptionHandler/ExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.openclassrooms.mddapi.exceptionHandler;
 
+import com.openclassrooms.mddapi.model.response.MessageResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
@@ -19,8 +21,8 @@ public class ExceptionHandler {
      * @return an error ResponseEntity.
      */
     @org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> handleValidationExceptions(MethodArgumentNotValidException exception) {
-        return new ResponseEntity<>(exception.getBindingResult().getAllErrors().get(0).getDefaultMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<MessageResponse> handleValidationExceptions(MethodArgumentNotValidException exception) {
+        return new ResponseEntity<>(new MessageResponse(exception.getBindingResult().getAllErrors().get(0).getDefaultMessage()), HttpStatus.BAD_REQUEST);
     }
 
     /**
@@ -30,7 +32,18 @@ public class ExceptionHandler {
      * @return an error ResponseEntity.
      */
     @org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    public ResponseEntity<String> handleValidationExceptions(MethodArgumentTypeMismatchException exception) {
-        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<MessageResponse> handleValidationExceptions(MethodArgumentTypeMismatchException exception) {
+        return new ResponseEntity<>(new MessageResponse(exception.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    /**
+     * Returns a bad request response when the request body can't be read.
+     *
+     * @param exception exception generated from the call.
+     * @return an error ResponseEntity.
+     */
+    @org.springframework.web.bind.annotation.ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<MessageResponse> handleValidationExceptions(HttpMessageNotReadableException exception) {
+        return new ResponseEntity<>(new MessageResponse(exception.getMessage()), HttpStatus.BAD_REQUEST);
     }
 }
